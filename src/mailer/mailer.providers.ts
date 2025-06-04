950703ea7bceaeab9a4080b931826c3291d97908
nghiemvanmanh <nvmanhfc2003@gmail.com>
2025-06-04 11:31:13 +0700

    feat:updateee

--- src/mailer/mailer.providers.ts
+++ src/mailer/mailer.providers.ts
@@ -1,17 +1,16 @@
 // src/mailer/mailer.providers.ts
 import * as nodemailer from 'nodemailer';
 import { Provider } from '@nestjs/common';
-import { EnvService } from 'src/env/env.service';
 
 export const MAILER_TOKEN = 'MAIL_TRANSPORTER';
 export const mailerProvider: Provider = {
   provide: MAILER_TOKEN,
-  useFactory: (env: EnvService) => {
+  useFactory: () => {
     return nodemailer.createTransport({
       service: 'gmail',
       auth: {
-        user: env.get('EMAIL_USER'),
-        pass: env.get('EMAIL_PASS'),
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
       },
     });
   },
